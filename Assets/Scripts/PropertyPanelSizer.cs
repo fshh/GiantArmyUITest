4bd5a67e52ca5442a4ce6fb13d5557328c31f057
fshh <ajknollmeyer@gmail.com>
2021-04-13 16:16:46 -0400

    Proper panel resizing with top/bottom margins

---
+++ Assets/Scripts/PropertyPanelSizer.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(RectTransform))]
+public class PropertyPanelSizer : MonoBehaviour
+{
+    public float TopMargin = 48f;
+    public float BottomMargin = 48f;
+    public RectTransform PropertiesRect;
+    public RectTransform PropertiesContent;
+
+    private float maxHeight { get => Screen.height - TopMargin - BottomMargin; }
+    private RectTransform rectTransform;
+
+    private void Awake()
+    {
+        rectTransform = GetComponent<RectTransform>();
+    }
+
+    private void Update()
+    {
+        float otherElementsHeight = rectTransform.sizeDelta.y - PropertiesRect.sizeDelta.y;
+        PropertiesRect.sizeDelta = new Vector2(PropertiesRect.sizeDelta.x, Mathf.Min(PropertiesContent.sizeDelta.y, maxHeight - otherElementsHeight));
+    }
+}
